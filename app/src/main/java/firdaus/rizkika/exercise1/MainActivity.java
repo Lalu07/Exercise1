70d21762d3ed544a6ed478c5cb93c4e414279407
Lalu Muhammad Rizkika Firdaus <iki070899@gmail.com>
2021-04-05 12:57:39 +0800

    deklarasi Text View dan Button, dan code buat pindah activity

--- app/src/main/java/firdaus/rizkika/exercise1/MainActivity.java
+++ app/src/main/java/firdaus/rizkika/exercise1/MainActivity.java
@@ -2,13 +2,32 @@ package firdaus.rizkika.exercise1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView Nama, Pass;
+    Button Masuk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Masuk = findViewById(R.id.LoginBtn);
+
+        Masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),dataKontak.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
