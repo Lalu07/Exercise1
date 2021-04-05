c0010ddffe79623223988c6c3d8ec103101786dd
Lalu Muhammad Rizkika Firdaus <iki070899@gmail.com>
2021-04-05 17:44:00 +0800

    Mengisi ListView

--- app/src/main/java/firdaus/rizkika/exercise1/dataKontak.java
+++ app/src/main/java/firdaus/rizkika/exercise1/dataKontak.java
@@ -3,12 +3,27 @@ package firdaus.rizkika.exercise1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class dataKontak extends AppCompatActivity {
+
+    String[] listNama;
+    ListView list;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_kontak);
+
+        listNama = new String[]{"Lalu","Muhammad","Rizkika","Firdaus","Yusuf","Kiki","Firja"};
+        ArrayAdapter<String> kontakAdapter = new ArrayAdapter<String>(dataKontak.this, android.R.layout.simple_expandable_list_item_1, listNama);
+
+        list = findViewById(R.id.listKontak);
+        list.setAdapter(kontakAdapter);
+
     }
 }
